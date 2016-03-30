12aa8a53d189e75df5c82947f27ec124b9a1b8f1
njscococo <njscococo@gmail.com>
2016-03-30 19:28:44 +0800

    add mongodb

---
+++ models/user.js
@@ -0,0 +1,69 @@
+var mongodb = require('./db');
+
+//定義User物件
+function User(user) {
+    this.name = user.name;
+    this.password = user.password;
+    this.email = user.email;
+};
+
+module.exports = User;
+
+User.prototype.save = function(callback) {
+    // user data
+    var user = {
+        name: this.name,
+        password: this.password,
+        email: this.email
+    };
+
+    //open DB connection
+    mongodb.open(function(err, db) {
+        if (err) {
+            //回傳error
+            return callback(err);
+        }
+
+        db.collection('users', function(err, collection) {
+            if (err) {
+                mongodb.close();
+                return callback(err);
+            }
+
+            //insert User data
+            collection.insert(user, {
+                safe: true
+            }, function(err, result) {
+            	mongodb.close();
+            	if(err){
+            		return callback(err);
+            	}
+            	callback(null, restul[0]);
+            });
+        });
+    });
+};
+
+//讀取User資料
+User.get = function(name , callback){
+	mongodb.open(function(err, db){
+		if(err){
+			return callback(err);
+		}
+		db.collection('user', function(err, collection){
+			if(err){
+				mongodb.close();
+				return callback(err);
+			}
+
+			//查詢user by Name
+			collection.findOne({name: name}, function(err, user){
+				mongodb.close();
+				if(err){
+					return callback(err);
+				}
+				callback(null, user);
+			});
+		});
+	});
+};
